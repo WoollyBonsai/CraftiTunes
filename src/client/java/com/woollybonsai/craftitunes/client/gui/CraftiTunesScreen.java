42d0b99b3df918dcfebd57dc17012abd75ee3a8d
WoollyBonsai <woollybonsai@users.noreply.github.com>
2026-07-15 14:23:05 +0530

    feat: dynamically load track list from test_music folder

--- src/client/java/com/woollybonsai/craftitunes/client/gui/CraftiTunesScreen.java
+++ src/client/java/com/woollybonsai/craftitunes/client/gui/CraftiTunesScreen.java
@@ -3,14 +3,19 @@ package com.woollybonsai.craftitunes.client.gui;
 import io.wispforest.owo.ui.base.BaseUIModelScreen;
 import io.wispforest.owo.ui.component.ButtonComponent;
 import io.wispforest.owo.ui.component.LabelComponent;
+import io.wispforest.owo.ui.component.Components;
 import io.wispforest.owo.ui.container.FlowLayout;
 import io.wispforest.owo.ui.core.Surface;
+import io.wispforest.owo.ui.core.Sizing;
+import io.wispforest.owo.ui.core.Insets;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
 import com.woollybonsai.craftitunes.audio.AudioEngine;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+import java.io.File;
 
 public class CraftiTunesScreen extends BaseUIModelScreen<FlowLayout> {
 
@@ -41,6 +46,33 @@ public class CraftiTunesScreen extends BaseUIModelScreen<FlowLayout> {
             nextBtn.onPress(button -> {
                 AudioEngine.getScheduler().nextTrack();
             });
+        }
+        
+        FlowLayout trackList = rootComponent.childById(FlowLayout.class, "track-list");
+        if (trackList != null) {
+            trackList.clearChildren();
+            
+            File musicDir = new File("/home/woolly/Work/Minecraft_Mods/Craftitunes/test_music");
+            if (musicDir.exists() && musicDir.isDirectory()) {
+                File[] files = musicDir.listFiles((d, name) -> name.endsWith(".mp3") || name.endsWith(".wav"));
+                if (files != null && files.length > 0) {
+                    for (File file : files) {
+                        ButtonComponent trackBtn = Components.button(
+                            Component.literal(file.getName()), 
+                            button -> {
+                                AudioEngine.loadAndPlay(file.getAbsolutePath());
+                            }
+                        );
+                        trackBtn.sizing(Sizing.fill(100), Sizing.fixed(20));
+                        trackBtn.margins(Insets.bottom(2));
+                        trackList.child(trackBtn);
+                    }
+                } else {
+                    trackList.child(Components.label(Component.literal("No tracks found.")));
+                }
+            } else {
+                trackList.child(Components.label(Component.literal("test_music folder not found.")));
+            }
         }
     }
 
